e42542dcc9900cd2ae612a404f13c07621edb21f
sakajamiu <sakaj20@gmail.com>
2021-08-14 23:43:36 +0100

    '.'

---
+++ courseinfo/src/App.js
@@ -0,0 +1,75 @@
+
+
+const Header = ({course})=> <h1>{course.name}</h1>
+
+const Part = ({part,exercises})=><p>{part} {exercises}</p>
+
+const Content =({course})=>{
+  return(
+    <div>
+      <Part
+        part = {course['parts'][0]['name']}
+        exercises = {course['parts'][0]['exercises']}
+      />
+      <Part
+        part = {course['parts'][1]['name']}
+        exercises = {course['parts'][1]['exercises']}
+       />
+      <Part 
+        part = {course['parts'][2]['name']} 
+        exercises = {course['parts'][2]['exercises']}
+      />
+    </div>
+  )
+
+}
+
+const Total = ({course})=>{
+  let total = 
+    course.parts[0].exercises + 
+    course.parts[1].exercises + 
+    course.parts[2].exercises
+
+
+  return(
+
+    <p>Number of exercises {total}</p>
+
+  )
+}
+   
+
+
+
+const App = () => {
+  
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Header course ={course}/>
+
+      <Content course = {course}/>
+
+      <Total course = {course}/>
+
+    </div>
+  )
+}
+export default App;
